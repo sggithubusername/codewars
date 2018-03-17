90e62712da8494a51e73e906fbdf40287aa63df4
grsrg <grsrg@protonmail.com>
2018-03-17 17:19:32 +0200

    Removed comments

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println("Hello World");
+        System.out.println("Hello World");
         Robot R = new Robot("Verter");
         R.greeting();
         int[] n = {-1,-2,100};
         System.out.println(SmallestIntegerFinder.findSmallestInt(n));
-        System.out.println(Order.order("is2 Thi1s T4est 3a")); */
+        System.out.println(Order.order("is2 Thi1s T4est 3a"));
         System.out.println( SumDigPower.sumDigPow(1,100).toString());
-
-
-
-
     }
 }
